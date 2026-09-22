dbb99beea02a44de9cb47a83bf4f50f712981bc6
Vas. Neratzis <vas.neratzis@gmail.com>
2026-09-22 19:33:28 +0300

    feat: add Product entity for inheritance

---
+++ src/main/java/com/neratzis/bookstore/model/Product.java
@@ -0,0 +1,69 @@
+package com.neratzis.bookstore.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Inheritance(strategy = InheritanceType.JOINED)
+@Table(name = "products")
+public abstract class Product extends AbstractEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false,unique = true)
+    private String sku;
+
+    private String brand;
+
+    private String dimensions;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    private BigDecimal discountPrice;
+
+    @Column(nullable = false)
+    private int stock;
+
+    private String imageUrl;
+    private boolean featured;
+
+    @Version
+    @Column(nullable = false)
+    private Long version;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Product product)) return false;
+        return Objects.equals(getSku(), product.getSku());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getSku());
+    }
+}
