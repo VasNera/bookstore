617e6db52e1741eea8ba2676048756692ba37a24
Vas. Neratzis <vas.neratzis@gmail.com>
2026-09-22 18:13:50 +0300

    feat: add Category entity

---
+++ src/main/java/com/neratzis/bookstore/model/Category.java
@@ -0,0 +1,65 @@
+package com.neratzis.bookstore.model;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "categories")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+
+
+    @OneToMany(mappedBy = "parentCategory", fetch = FetchType.LAZY)
+    private Set<Category> subCategories = new HashSet<>();
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_category_id")
+    private Category parentCategory;
+
+    public void addSubCategory(Category category){
+        this.subCategories.add(category);
+        category.setParentCategory(this);
+    }
+
+    public void removeSubCategory(Category category){
+        this.subCategories.remove(category);
+        category.setParentCategory(null);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Category category)) return false;
+        return Objects.equals(getName(), category.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getName());
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + getId() +
+                ", name='" + getName() + '\'' +
+                '}';
+    }
+}
