35569527fafee6dd7509fec95a779ac9c1630bfd
Vas. Neratzis <vas.neratzis@gmail.com>
2026-09-23 15:25:59 +0300

    feat: add Order entity

---
+++ src/main/java/com/neratzis/bookstore/model/Order.java
@@ -0,0 +1,81 @@
+package com.neratzis.bookstore.model;
+
+
+import com.neratzis.bookstore.model.enums.DocumentType;
+import com.neratzis.bookstore.model.enums.OrderStatus;
+import com.neratzis.bookstore.model.enums.PaymentMethod;
+import com.neratzis.bookstore.model.enums.PaymentStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "orders")
+public class Order extends AbstractEntity {
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private DocumentType documentType;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PaymentMethod paymentMethod;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PaymentStatus paymentStatus;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private OrderStatus orderStatus;
+
+    @Column(nullable = false, unique = true)
+    private String orderNumber;
+
+
+    private String billingBusinessName;
+
+
+    private String billingTaxNumber;
+
+
+    private String billingStreet;
+
+
+    private String billingProfession;
+
+    private String billingTaxOffice;
+
+    @Column(nullable = false)
+    private String shippingStreet;
+
+    @Column(nullable = false)
+    private String shippingCity;
+
+    @Column(nullable = false)
+    private String shippingPostalCode;
+
+    @Column(columnDefinition = "TEXT")
+    private String notes;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal totalAmount;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @OneToMany(mappedBy = "order", fetch = FetchType.LAZY)
+    private Set<OrderItem> orderItems = new HashSet<>();
+
+}
