d57cb76ceed6c8e836fb5442a04708f2891239a0
Vas. Neratzis <vas.neratzis@gmail.com>
2026-09-23 12:52:29 +0300

    feat: add enum for PaymentStatus and PaymentMethod

---
+++ src/main/java/com/neratzis/bookstore/model/enums/PaymentMethod.java
@@ -0,0 +1,9 @@
+package com.neratzis.bookstore.model.enums;
+
+public enum PaymentMethod {
+
+    CARD,
+    PAYPAL,
+    BANK_TRANSFER,
+    CASH_ON_DELIVERY
+}
---
+++ src/main/java/com/neratzis/bookstore/model/enums/PaymentStatus.java
@@ -0,0 +1,9 @@
+package com.neratzis.bookstore.model.enums;
+
+public enum PaymentStatus {
+
+    PENDING,
+    PAID,
+    FAILED,
+    REFUNDED
+}
