76fdaa44b5ca7422fb1beb775e20989316259f85
Vas. Neratzis <vas.neratzis@gmail.com>
2026-09-25 15:48:46 +0300

    feat: add Review entity

---
+++ src/main/java/com/neratzis/bookstore/model/Review.java
@@ -0,0 +1,39 @@
+package com.neratzis.bookstore.model;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "reviews")
+public class Review extends AbstractEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "TEXT")
+    private String comment;
+
+    @Min(1)
+    @Max(5)
+    @Column(nullable = false)
+    private int rating;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+}
