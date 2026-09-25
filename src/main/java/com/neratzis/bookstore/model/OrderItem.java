7b1e4d48733a79c3da5bc05c1ae870d111c58cf4
Vas. Neratzis <vas.neratzis@gmail.com>
2026-09-25 15:26:51 +0300

    feat: add OrderItem entity

---
+++ src/main/java/com/neratzis/bookstore/model/OrderItem.java
@@ -0,0 +1,40 @@
+package com.neratzis.bookstore.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "order_items")
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal priceAtPurchase;
+
+    @Column(nullable = false)
+    private String productTitle;
+
+    @Column(nullable = false)
+    private int quantity;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+}
