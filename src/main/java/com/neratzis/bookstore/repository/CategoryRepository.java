d185b66f635323a7c36d586a49df90887dd3bbed
Vas. Neratzis <vas.neratzis@gmail.com>
2026-09-25 18:43:08 +0300

    feat: add CategoryRepository

---
+++ src/main/java/com/neratzis/bookstore/repository/CategoryRepository.java
@@ -0,0 +1,16 @@
+package com.neratzis.bookstore.repository;
+
+import com.neratzis.bookstore.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    Optional<Category> findByName(String name);
+
+    List<Category> findAllByParentCategory(Category parentCategory);
+
+    boolean existsByName(String name);
+}
