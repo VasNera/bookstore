649b072a96dbd9edd522442f067a4db770aba7db
Vas. Neratzis <vas.neratzis@gmail.com>
2026-09-26 16:25:44 +0300

    feat: add CartItemRepository

---
+++ src/main/java/com/neratzis/bookstore/repository/CartItemRepository.java
@@ -0,0 +1,14 @@
+package com.neratzis.bookstore.repository;
+
+import com.neratzis.bookstore.model.Cart;
+import com.neratzis.bookstore.model.CartItem;
+import com.neratzis.bookstore.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+
+    Optional<CartItem> findByCartAndProduct(Cart cart, Product product);
+
+}
